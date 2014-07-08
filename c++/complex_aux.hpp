4f027f0e77354684bb4335030f5b9221ad21ddf0
Igor Krivenko <iskrivenko@proton.me>
2014-07-08 13:57:23 +0200

    Integration of many_body_operator with time_expr. Step 2.
    time_expr and complex time_expr have been rewritten.

---
+++ c++/complex_aux.hpp
@@ -0,0 +1,215 @@
+#pragma once
+
+#include <complex>
+#include <limits>
+#include <type_traits>
+#include <utility>
+#include <tuple>
+#include <ostream>
+#include <triqs/utility/is_complex.hpp>
+#include <boost/serialization/complex.hpp>
+#include <boost/operators.hpp>
+
+namespace realevol {
+
+// Generic tests for being zero 
+template<class T>
+typename std::enable_if<std::is_integral<T>::value,bool>::type is_zero(T x)
+{
+    return x==0;
+}
+template<class T>
+typename std::enable_if<std::is_floating_point<T>::value,bool>::type is_zero(T x)
+{
+    return std::fabs(x) < 100*std::numeric_limits<T>::epsilon();
+}
+
+template<class T>
+typename std::enable_if<triqs::is_complex<T>::value,bool>::type is_zero(T x)
+{
+    return is_zero(std::real(x)) && is_zero(std::imag(x));
+}
+
+// Generic complex conjugate
+template<class T>
+typename std::enable_if<std::is_arithmetic<T>::value,T>::type _conj(T x)
+{
+    return x;
+}
+
+template<class T>
+typename std::enable_if<triqs::is_complex<T>::value,T>::type _conj(T x)
+{
+    return std::conj(x);
+}
+
+// Check for being constant
+template<class T> bool is_constant(T x) { return true; }
+
+// Callable complex type
+template<class T>
+class callable_complex : public boost::operators<callable_complex<T>> {
+    
+    T re, im;
+    
+    template<typename X>
+    struct _T_compatible : public std::is_convertible<typename std::decay<X>::type,T> {};
+    
+    template<typename X>
+    struct _is_complex : public std::integral_constant<bool,
+                std::is_same<typename std::decay<X>::type,callable_complex>::value ||
+                (triqs::is_complex<X>::value && _T_compatible<typename X::value_type>::value)
+                > {};
+    
+public:
+    
+    using value_type = T;
+
+    // Constructors
+    callable_complex() : re(.0), im(.0) {}
+    template<typename R, typename I,
+             typename std::enable_if<_T_compatible<R>::value && _T_compatible<I>::value,bool>::type = false>
+    callable_complex(R r, I i) : re(r), im(i)
+    {}
+    template<typename C,
+             typename std::enable_if<triqs::is_complex<C>::value && _T_compatible<typename C::value_type>::value,
+             bool>::type = false>
+    callable_complex(C z) : re(z.real()), im(z.imag())
+    {}    
+    template<typename R,
+             typename std::enable_if<_T_compatible<R>::value,bool>::type = false>
+    callable_complex(R r) : re(r), im(.0)
+    {}
+
+    // Copy-constructor, move-constructor & assignment
+    callable_complex(callable_complex const&) = default;
+    callable_complex(callable_complex &&) = default;
+    callable_complex & operator=(callable_complex const&) = default;
+    
+    template<typename R,
+             typename std::enable_if<_T_compatible<R>::value,bool>::type = false>
+    callable_complex & operator=(R r)
+    {
+        re = r;
+        im = .0;
+        return *this;
+    }
+    
+    // Forward the call to the real and imaginary parts
+    template<typename... Args>
+    auto operator()(Args&&... args) -> std::complex<typename std::result_of<T(Args...)>::type>
+    {
+        return {re(std::forward<Args>(args)...),im(std::forward<Args>(args)...)};
+    }
+        
+    // Unary minus
+    callable_complex operator-() const
+    {
+        return callable_complex(-re,-im);
+    }
+    
+    // Addition
+    template<typename C,typename std::enable_if<_is_complex<C>::value,bool>::type = false>
+    callable_complex operator+=(C z)
+    {
+        re += z.real();
+        im += z.imag();
+        return *this;        
+    }
+    
+    template<typename U,typename std::enable_if<_T_compatible<U>::value,bool>::type = false>
+    callable_complex operator+=(U x)
+    {
+        re += x.re;
+        return *this;
+    }
+   
+    // Subtraction
+    template<typename C,typename std::enable_if<_is_complex<C>::value,bool>::type = false>
+    callable_complex operator-=(C z)
+    {
+        re -= z.real();
+        im -= z.imag();
+        return *this;        
+    }
+    
+    template<typename U,typename std::enable_if<_T_compatible<U>::value,bool>::type = false>
+    callable_complex operator-=(U x)
+    {
+        re -= x.re;
+        return *this;
+    }
+    
+    // Multiplication
+    template<typename C,typename std::enable_if<_is_complex<C>::value,bool>::type = false>
+    callable_complex operator*=(C z)
+    {
+        std::tie(re,im) = std::make_tuple(re*z.real() - im*z.imag(),
+                                          re*z.imag() + im*z.real());
+        return *this;        
+    }
+    
+    template<typename U,typename std::enable_if<_T_compatible<U>::value,bool>::type = false>
+    callable_complex operator*=(U x)
+    {
+        re *= x;
+        im *= x;
+        return *this;
+    }
+    
+    // Division
+    template<typename C,typename std::enable_if<_is_complex<C>::value,bool>::type = false>
+    callable_complex operator/=(C z)
+    {
+        T abs2 = z.real()*z.real() + z.imag()*z.imag();
+        std::tie(re,im) = std::make_tuple((re*z.real() + im*z.imag())/abs2,
+                                          (im*z.real() - re*z.imag())/abs2);
+        return *this;        
+    }
+    
+    template<typename U,typename std::enable_if<_T_compatible<U>::value,bool>::type = false>
+    callable_complex operator/=(U x)
+    {
+        re /= x;
+        im /= x;
+        return *this;
+    }    
+
+    value_type real() const { return re; }
+    value_type imag() const { return im; }
+    
+    bool operator==(callable_complex const& z) const
+    {
+        return re==z.re && im == z.im;
+    }
+    
+    friend bool is_constant(callable_complex const& z)
+    {
+        return is_constant(z.re) && is_constant(z.im);
+    }
+    
+    friend std::ostream& operator<<(std::ostream& os, callable_complex const& z)
+    {
+        os << "(" << z.re << "," << z.im << ")";
+        return os;
+    }
+    
+    friend value_type _conj(callable_complex z)
+    {
+        return callable_complex(z.re,-z.im);
+    }
+        
+private:
+    
+    // Boost serialization
+    friend class boost::serialization::access;
+    template<class Archive>
+    void serialize(Archive & ar, const unsigned int version)
+    {
+        // serialize base class information
+        ar & re;
+        ar & im;
+    }
+};
+
+}
---
+++ c++/time_expr.cpp
@@ -0,0 +1,248 @@
+#include "time_expr.hpp"
+
+#include <limits>
+#include <deque>
+#include <boost/algorithm/string/trim.hpp>
+
+using boost::lexical_cast;
+using boost::trim_copy;
+
+namespace realevol {
+
+// Singleton pool of mu::Parser objects
+class muParser_pool {
+    std::deque<mu::Parser*> parsers;
+    static constexpr const std::size_t chunk_size = 5;
+
+public:
+    mu::Parser* acquire()
+    {
+        if(!parsers.size()){
+            for(std::size_t n=0; n<chunk_size; ++n) parsers.push_back(new mu::Parser);
+        }
+        auto p = parsers.back();
+        parsers.pop_back();
+        return p;
+    }
+
+    void release(mu::Parser* p)
+    {
+        if(p!=nullptr) parsers.push_back(p);
+    }
+} pool;
+
+time_expr::time_expr(std::string const& expr) :
+    parser(pool.acquire())
+{
+    parser->DefineVar("t", &arg);
+    parser->SetExpr(trim_copy(expr));
+    if(!parser->GetUsedVar().size()){
+        arg = parser->Eval();
+        pool.release(parser);
+        parser = nullptr;
+    }
+}
+
+time_expr::time_expr(const char* expr) : time_expr(std::string(expr))
+{}
+
+time_expr::time_expr(time_expr::result_type r) :
+    arg(r), parser(nullptr)
+{}
+
+time_expr::time_expr() : time_expr(result_type(0))
+{}
+
+time_expr::time_expr(time_expr const& te) :
+    arg(te.arg), parser(nullptr)
+{
+    if(te.parser != nullptr){
+        parser = pool.acquire();
+        parser->DefineVar("t",&arg);
+        parser->SetExpr(trim_copy(te.parser->GetExpr()));
+    }
+}
+
+time_expr::~time_expr()
+{
+    pool.release(parser);
+}
+
+auto time_expr::operator()(double t) const -> result_type
+{
+    if(is_constant(*this))
+        return arg;
+    else {
+        arg = t;
+        return parser->Eval();
+    }
+}
+
+std::ostream& operator<<(std::ostream& os, time_expr const& te)
+{
+    if(is_constant(te))
+        os << te.arg;
+    else
+        os << trim_copy(te.parser->GetExpr());
+    
+    return os;
+}
+
+auto time_expr::operator-() const -> time_expr
+{
+    if(is_constant(*this))
+        return time_expr(-arg);
+    else
+        return time_expr("-(" + parser->GetExpr() + ")");
+}
+
+auto time_expr::operator=(const time_expr& te) -> time_expr
+{
+    if(is_constant(te)){
+        arg = te.arg;
+        pool.release(parser);
+        parser = nullptr;
+    }else{
+        if(is_constant(*this)){
+            parser = pool.acquire();
+            parser->DefineVar("t",&arg);            
+        }
+        parser->SetExpr(trim_copy(te.parser->GetExpr()));
+    }
+    return *this;
+}
+
+auto time_expr::operator=(std::string const& expr) -> time_expr
+{
+    if(is_constant(*this))
+    {
+        parser = pool.acquire();
+    }
+    parser->DefineVar("t", &arg);
+    parser->SetExpr(trim_copy(expr));
+    if(!parser->GetUsedVar().size()){
+        arg = parser->Eval();
+        pool.release(parser);
+        parser = nullptr;
+    }
+    
+    return *this;
+}
+
+auto time_expr::operator=(const char* expr) -> time_expr
+{
+    *this = std::string(expr);
+    
+    return *this;
+}
+
+auto time_expr::operator=(result_type r) -> time_expr
+{
+    if(!is_constant(*this)){
+        pool.release(parser);
+        parser = nullptr;
+    }
+    arg = r;
+    
+    return *this;
+}
+
+auto time_expr::operator+=(const time_expr& te) -> time_expr
+{
+    bool is_const = is_constant(*this);
+    if(is_const && is_constant(te))
+        arg += te.arg;
+    else {
+        if(is_const){
+            parser = pool.acquire();
+            parser->DefineVar("t",&arg);
+        }
+        parser->SetExpr(
+            "(" +
+            (is_const ? lexical_cast<std::string>(arg) : trim_copy(parser->GetExpr())) +
+            ")+(" +
+            (is_constant(te) ? lexical_cast<std::string>(te.arg) : trim_copy(te.parser->GetExpr())) +
+            ")"
+        );
+    }
+    
+    return *this;
+}
+
+auto time_expr::operator-=(const time_expr& te) -> time_expr
+{
+    bool is_const = is_constant(*this);
+    if(is_const && is_constant(te))
+        arg -= te.arg;
+    else {
+        if(is_const){
+            parser = pool.acquire();
+            parser->DefineVar("t",&arg);
+        }
+        parser->SetExpr(
+            "(" +
+            (is_const ? lexical_cast<std::string>(arg) : trim_copy(parser->GetExpr())) +
+            ")-(" +
+            (is_constant(te) ? lexical_cast<std::string>(te.arg) : trim_copy(te.parser->GetExpr())) +
+            ")"
+        );
+    }
+    
+    return *this;
+}
+
+auto time_expr::operator*=(const time_expr& te) -> time_expr
+{
+    bool is_const = is_constant(*this);
+    if(is_const && is_constant(te))
+        arg *= te.arg;
+    else {
+        if(is_const){
+            parser = pool.acquire();
+            parser->DefineVar("t",&arg);
+        }
+        parser->SetExpr(
+            "(" +
+            (is_const ? lexical_cast<std::string>(arg) : trim_copy(parser->GetExpr())) +
+            ")*(" +
+            (is_constant(te) ? lexical_cast<std::string>(te.arg) : trim_copy(te.parser->GetExpr())) +
+            ")"
+        );
+    }
+    
+    return *this;
+}
+
+auto time_expr::operator/=(const time_expr& te) -> time_expr
+{
+    bool is_const = is_constant(*this);
+    if(is_const && is_constant(te))
+        arg /= te.arg;
+    else {
+        if(is_const){
+            parser = pool.acquire();
+            parser->DefineVar("t",&arg);
+        }
+        parser->SetExpr(
+            "(" +
+            (is_const ? lexical_cast<std::string>(arg) : trim_copy(parser->GetExpr())) +
+            ")/(" +
+            (is_constant(te) ? lexical_cast<std::string>(te.arg) : trim_copy(te.parser->GetExpr())) +
+            ")"
+        );
+    }
+    
+    return *this;
+}
+
+bool time_expr::operator==(const time_expr& te) const
+{
+    bool is_const = is_constant(*this);
+    if(is_const != is_constant(te)) return false;
+    if(is_const)
+        return is_zero(arg-te.arg);
+    else
+        return parser->GetExpr() == te.parser->GetExpr();
+}
+    
+}
