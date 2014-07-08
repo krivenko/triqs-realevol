1b32c302041955ada9af5ef6c041eedeca5e9f0b
Igor Krivenko <igor.s.krivenko@gmail.com>
2014-07-08 13:57:23 +0200

    Integration of many_body_operator with time_expr. Step 2.
    time_expr and complex time_expr have been rewritten.

--- c++/time_expr.hpp
+++ c++/time_expr.hpp
@@ -2,7 +2,6 @@
 
 #pragma once
 
-#include <complex>
 #include <string>
 #include <ostream>
 #include <limits>
@@ -12,13 +11,12 @@
 
 #include <boost/serialization/string.hpp>
 #include <boost/serialization/split_member.hpp>
-#include <boost/serialization/complex.hpp>
 #include <boost/lexical_cast.hpp>
 
 #include <boost/serialization/string.hpp>
 #include <boost/serialization/split_member.hpp>
 
-#include "numerics.hpp"
+#include "complex_aux.hpp"
 #include "mesh_base.hpp"
 #include "mesh_iterator.hpp"
 
@@ -26,11 +24,9 @@ using boost::lexical_cast;
 
 namespace realevol {
     
-// Time-dependent expressions
-template<bool ComplexValued = false> class time_expr;
+// Time-dependent expressions;
     
-template<>
-class time_expr<false> : public boost::operators<time_expr<false>>
+class time_expr : public boost::operators<time_expr>
 {
 
 public:
@@ -44,11 +40,14 @@ public:
     time_expr(time_expr const& te);
     ~time_expr();
     result_type operator()(double t) const;
-    bool is_constant() const;
-
+    
+    friend bool is_constant(time_expr const& te)
+    {
+        return te.parser==nullptr;
+    }
     friend bool is_zero(time_expr const& te)
     {
-        return te.is_constant() && is_zero(te.arg);
+        return te.parser==nullptr && is_zero(te.arg);
     }
     
     // Stream output
@@ -57,16 +56,15 @@ public:
     time_expr operator-() const;
     
     time_expr operator=(time_expr const& te);
+    time_expr operator=(std::string const& expr);
+    time_expr operator=(const char* expr);
+    time_expr operator=(result_type r);
+    
     time_expr operator+=(time_expr const& te);
     time_expr operator-=(time_expr const& te);
     time_expr operator*=(time_expr const& te);
     time_expr operator/=(time_expr const& te);
     bool operator==(time_expr const& te) const;
-    
-    friend time_expr _conj(time_expr const& te)
-    {
-        return te;
-    }
     
 private:
     
@@ -79,7 +77,7 @@ private:
     template<class Archive>
     void save(Archive & ar, const unsigned int version) const
     {
-        ar & (is_constant() ? lexical_cast<std::string>(arg) : parser->GetExpr());
+        ar & (is_constant(*this) ? lexical_cast<std::string>(arg) : parser->GetExpr());
     }
     
     template<class Archive>
@@ -92,87 +90,11 @@ private:
     BOOST_SERIALIZATION_SPLIT_MEMBER()
 };
 
-// Time-dependent expressions (complex)
-template<>
-class time_expr<true> : public boost::operators<time_expr<true>>
-{
-
-public:
-    
-    using result_type = std::complex<double>;
-    
-    time_expr();
-    time_expr(std::string const& expr_re, std::string const& expr_im = "0");
-    time_expr(const char* expr_re, const char* expr_im = "0");
-    time_expr(result_type c);
-    time_expr(result_type::value_type r);
-    time_expr(time_expr const& te);
-    result_type operator()(double t) const;
-    bool is_constant() const;
-
-    friend bool is_zero(time_expr const& te)
-    {
-        return te.is_constant() && is_zero(te.precomputed_value);
-    }
-    
-    // Stream output
-    friend std::ostream& operator<<(std::ostream& os, time_expr<true> const& te);
-
-    time_expr operator-() const;
-    
-    time_expr operator=(time_expr const& te);
-    time_expr operator+=(time_expr const& te);
-    time_expr operator-=(time_expr const& te);
-    time_expr operator*=(time_expr const& te);
-    time_expr operator/=(time_expr const& te);
-    bool operator==(time_expr const& te) const;
-    
-    friend time_expr _conj(time_expr const& te)
-    {
-        if(te.is_constant())
-            return _conj(te.precomputed_value);
-        else
-            return time_expr(te.parser_re.GetExpr(),"-(" + te.parser_im.GetExpr() + ")");
-    }
-    
-private:
-    
-    mu::Parser parser_re, parser_im;
-    
-    mutable double t;
-    
-    mutable bool _is_constant;
-    mutable result_type precomputed_value;
-    
-    // Methods for boost::serialization
-    friend class boost::serialization::access;
-
-    template<class Archive>
-    void save(Archive & ar, const unsigned int version) const
-    {
-        ar & parser_re.GetExpr();
-        ar & parser_im.GetExpr();
-        ar & _is_constant;
-        if(_is_constant) ar & precomputed_value;
-    }
-    
-    template<class Archive>
-    void load(Archive & ar, const unsigned int version)
-    {
-        std::string expr;
-        ar & expr;
-        parser_re.SetExpr(expr);
-        ar & expr;
-        parser_im.SetExpr(expr);
-        ar & _is_constant;
-        if(_is_constant) ar & precomputed_value;
-    }
-    BOOST_SERIALIZATION_SPLIT_MEMBER()
-};
+using complex_time_expr = callable_complex<time_expr>;
 
 // Replace the expression with a constant if it takes equal values at all mesh points
-template<class Mesh, bool ComplexValued>
-bool try_reduce_to_constant(time_expr<ComplexValued>& te, Mesh const& m)
+template<class Mesh, class Expr>
+bool try_reduce_to_constant(Expr& te, Mesh const& m)
 {
     auto it = m.begin();
     auto value = te(*it);
@@ -180,7 +102,7 @@ bool try_reduce_to_constant(time_expr<ComplexValued>& te, Mesh const& m)
     for(it++; it != m.end(); it++)
         if(!is_zero(te(*it) - value)) return false;
         
-    te = time_expr<ComplexValued>(value);
+    te = Expr(value);
     return true;
 }
 
--- test/c++/time_expr.cpp
+++ test/c++/time_expr.cpp
@@ -14,13 +14,221 @@ int main()
 {   
     {
     // Test real-valued expressions
-    using time_expr = time_expr<false>;
-    #include "time_expr.body.hpp"
+    using expr_t = time_expr;
+
+    expr_t te1("t^2"), te2("t + sin(_pi/2)"), te3("sqrt(9.0) + 1.5");
+
+    // Check whether the expressions really depend on time
+    if(is_constant(te1) || is_constant(te2) || !is_constant(te3))
+        return EXIT_FAILURE;
+    
+    // Write to a archive
+    std::stringstream archive_str;
+    boost::archive::text_oarchive oa(archive_str);
+    oa << te1; oa << te2; oa << te3;
+    
+    // Read from an archive
+    boost::archive::text_iarchive ia(archive_str);
+    expr_t read_expr;
+    ia >> read_expr; if(read_expr != te1) return EXIT_FAILURE;
+    ia >> read_expr; if(read_expr != te2) return EXIT_FAILURE;
+    ia >> read_expr; if(read_expr != te3) return EXIT_FAILURE;
+    
+    // Check correctness of numerical expressions
+    double T[] = {0, 0.1, 10, 55};
+    double TE1_res[] = {0, 0.01, 100, 3025};
+    double TE2_res[] = {1, 1.1, 11, 56};
+    double TE3_res[] = {4.5, 4.5, 4.5, 4.5};
+
+    for(int i = 0; i < 4; ++i){
+        if(std::abs(te1(T[i]) - TE1_res[i]) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(te2(T[i]) - TE2_res[i]) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(te3(T[i]) - TE3_res[i]) >= 1e-10) return EXIT_FAILURE;
+    }
+
+    // Unary minus
+    expr_t mte2 = -te2;
+    for(int i = 0; i < 4; ++i){
+        if(std::abs(mte2(T[i]) - (-TE2_res[i])) >= 1e-10) return EXIT_FAILURE;
+    }
+    
+    // Addition of expressions
+    expr_t te1pte2 = te1 + te2;
+    expr_t te1phalf = te1 + 0.5;
+    expr_t halfpte2 = 0.5 + te2;
+
+    for(int i = 0; i < 4; ++i){
+        if(std::abs(te1pte2(T[i]) - (TE1_res[i]+TE2_res[i])) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(te1phalf(T[i]) - (TE1_res[i]+0.5)) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(halfpte2(T[i]) - (0.5+TE2_res[i])) >= 1e-10) return EXIT_FAILURE;
+    }
+    
+    // Subtraction of expressions
+    expr_t te1mte2 = te1 - te2;
+    expr_t te1mhalf = te1 - 0.5;
+    expr_t halfmte2 = 0.5 - te2;
+    for(int i = 0; i < 4; ++i){
+        if(std::abs(te1mte2(T[i]) - (TE1_res[i]-TE2_res[i])) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(te1mhalf(T[i]) - (TE1_res[i]-0.5)) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(halfmte2(T[i]) - (0.5-TE2_res[i])) >= 1e-10) return EXIT_FAILURE;
+    }
+   
+    // Multiplication of expressions
+    expr_t te1ppte2 = te1 * te2;
+    expr_t te1pphalf = te1 * 0.5;
+    expr_t halfppte2 = 0.5 * te2;
+    for(int i = 0; i < 4; ++i){
+        if(std::abs(te1ppte2(T[i]) - (TE1_res[i]*TE2_res[i])) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(te1pphalf(T[i]) - (TE1_res[i]*0.5)) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(halfppte2(T[i]) - (0.5*TE2_res[i])) >= 1e-10) return EXIT_FAILURE;
+    }
+    
+    // Division of expressions 
+    expr_t te1dte2 = te1 / te2;
+    expr_t te1dhalf = te1 / 0.5;
+    expr_t halfdte2 = 0.5 / te2;
+    for(int i = 0; i < 4; ++i){
+        if(std::abs(te1dte2(T[i]) - (TE1_res[i]/TE2_res[i])) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(te1dhalf(T[i]) - (TE1_res[i]/0.5)) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(halfdte2(T[i]) - (0.5/TE2_res[i])) >= 1e-10) return EXIT_FAILURE;
+    }    
+    
+    // Test try_reduce_to_constant()
+    expr_t te0t("0*t"), te1t("1*t");
+    if(is_constant(te0t) || is_constant(te1t)) return EXIT_FAILURE;
+    uniform_mesh<> m(0,100,101);
+    try_reduce_to_constant(te0t, m);
+    try_reduce_to_constant(te1t, m);
+    if(!is_constant(te0t)) return EXIT_FAILURE;
+    if(is_constant(te1t)) return EXIT_FAILURE;
+
+    // Test assignments
+    expr_t tea;
+    tea = te1t; if(tea != te1t) return EXIT_FAILURE;
+    tea = "1*t"; if(tea != te1t) return EXIT_FAILURE;
+    tea = std::string("1*t"); if(tea != te1t) return EXIT_FAILURE;
+    tea = .0; if(tea != te0t) return EXIT_FAILURE;
     }
     {
     // Test complex-valued expressions
-    using time_expr = time_expr<true>;
-    #include "time_expr.body.hpp"
+    using expr_t = complex_time_expr;
+    
+    const std::complex<double> I(0,1);
+
+    expr_t te1("t^2",1.0), te2("t + sin(_pi/2)","t^3");
+    expr_t te3("sqrt(9.0) + 1.5","2.9"), te4(1.9+I*2.8);
+
+    // Check whether the expressions really depend on time
+    if(is_constant(te1) || is_constant(te2) || !is_constant(te3) || !is_constant(te4))
+        return EXIT_FAILURE;
+    
+    // Write to a archive
+    std::stringstream archive_str;
+    boost::archive::text_oarchive oa(archive_str);
+    oa << te1; oa << te2; oa << te3;
+    
+    // Read from an archive
+    boost::archive::text_iarchive ia(archive_str);
+    expr_t read_expr;
+    ia >> read_expr; if(read_expr != te1) return EXIT_FAILURE;
+    ia >> read_expr; if(read_expr != te2) return EXIT_FAILURE;
+    ia >> read_expr; if(read_expr != te3) return EXIT_FAILURE;
+    
+    // Test assignments
+    expr_t tea;
+    tea = expr_t("t^2",1.0); if(tea != expr_t("t^2",1.0)) return EXIT_FAILURE;
+    tea = 0.7; if(tea != expr_t("0.7")) return EXIT_FAILURE;
+    tea = std::string("2*t+4*t^3"); if(tea != expr_t("2*t+4*t^3")) return EXIT_FAILURE;
+    tea = "8-t"; if(tea != expr_t("8-t")) return EXIT_FAILURE;
+    tea = {2.0,5.0}; if(tea != expr_t("2.0","5.0")) return EXIT_FAILURE;
+    if(tea != expr_t(2.0,5.0)) return EXIT_FAILURE;
+    
+    // Check correctness of numerical expressions
+    double T[] = {0, 0.1, 10, 55};
+    std::complex<double> TE1_res[] = {{0,1.0}, {0.01,1.0}, {100,1.0}, {3025,1.0}};
+    std::complex<double> TE2_res[] = {{1,0}, {1.1,0.001}, {11,1000}, {56,166375}};
+    std::complex<double> TE3_res[] = {{4.5,2.9}, {4.5,2.9}, {4.5,2.9}, {4.5,2.9}};
+    std::complex<double> TE4_res[] = {{1.9,2.8}, {1.9,2.8}, {1.9,2.8}, {1.9,2.8}};
+
+    for(int i = 0; i < 4; ++i){
+        if(std::abs(te1(T[i]) - TE1_res[i]) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(te2(T[i]) - TE2_res[i]) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(te3(T[i]) - TE3_res[i]) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(te4(T[i]) - TE4_res[i]) >= 1e-10) return EXIT_FAILURE;
+    }
+
+    // Unary minus
+    expr_t mte2 = -te2;
+    for(int i = 0; i < 4; ++i){
+        if(std::abs(mte2(T[i]) - (-TE2_res[i])) >= 1e-10) return EXIT_FAILURE;
+    }
+    
+    // Addition of expressions
+    expr_t te1pte2 = te1 + te2;
+    expr_t te1phalf = te1 + 0.5;
+    expr_t halfpte2 = 0.5 + te2;
+    expr_t te1pihalf = te1 + 0.5*I;
+    expr_t ihalfpte2 = 0.5*I + te2;
+
+    for(int i = 0; i < 4; ++i){
+        if(std::abs(te1pte2(T[i]) - (TE1_res[i]+TE2_res[i])) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(te1phalf(T[i]) - (TE1_res[i]+0.5)) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(halfpte2(T[i]) - (0.5+TE2_res[i])) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(te1pihalf(T[i]) - (TE1_res[i]+0.5*I)) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(ihalfpte2(T[i]) - (0.5*I+TE2_res[i])) >= 1e-10) return EXIT_FAILURE;
+    }
+
+    // Subtraction of expressions
+    expr_t te1mte2 = te1 - te2;
+    expr_t te1mhalf = te1 - 0.5;
+    expr_t halfmte2 = 0.5 - te2;
+    expr_t te1mihalf = te1 - 0.5*I;
+    expr_t ihalfmte2 = 0.5*I - te2;
+    for(int i = 0; i < 4; ++i){
+        if(std::abs(te1mte2(T[i]) - (TE1_res[i]-TE2_res[i])) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(te1mhalf(T[i]) - (TE1_res[i]-0.5)) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(halfmte2(T[i]) - (0.5-TE2_res[i])) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(te1mihalf(T[i]) - (TE1_res[i]-0.5*I)) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(ihalfmte2(T[i]) - (0.5*I-TE2_res[i])) >= 1e-10) return EXIT_FAILURE;        
+    }
+    
+    // Multiplication of expressions
+    expr_t te1ppte2 = te1 * te2;
+    expr_t te1pphalf = te1 * 0.5;
+    expr_t halfppte2 = 0.5 * te2;
+    expr_t te1ppihalf = te1 * 0.5*I;
+    expr_t ihalfppte2 = 0.5*I * te2;
+    for(int i = 0; i < 4; ++i){
+        if(std::abs(te1ppte2(T[i]) - (TE1_res[i]*TE2_res[i])) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(te1pphalf(T[i]) - (TE1_res[i]*0.5)) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(halfppte2(T[i]) - (0.5*TE2_res[i])) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(te1ppihalf(T[i]) - (TE1_res[i]*0.5*I)) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(ihalfppte2(T[i]) - (0.5*I*TE2_res[i])) >= 1e-10) return EXIT_FAILURE;        
+    }
+    
+    // Division of expressions 
+    expr_t te1dte2 = te1 / te2;
+    expr_t te1dhalf = te1 / 0.5;
+    expr_t halfdte2 = 0.5 / te2;
+    expr_t te1dihalf = te1 / (0.5*I);
+    expr_t ihalfdte2 = (0.5*I) / te2;    
+    for(int i = 0; i < 4; ++i){
+        if(std::abs(te1dte2(T[i]) - (TE1_res[i]/TE2_res[i])) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(te1dhalf(T[i]) - (TE1_res[i]/0.5)) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(halfdte2(T[i]) - (0.5/TE2_res[i])) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(te1dihalf(T[i]) - (TE1_res[i]/(0.5*I))) >= 1e-10) return EXIT_FAILURE;
+        if(std::abs(ihalfdte2(T[i]) - ((0.5*I)/TE2_res[i])) >= 1e-10) return EXIT_FAILURE;
+    }    
+    
+    // Test try_reduce_to_constant()
+    expr_t te0t("0*t","2"), te1t("1*t","t^3");
+    if(is_constant(te0t) || is_constant(te1t)) return EXIT_FAILURE;
+    uniform_mesh<> m(0,100,101);
+    try_reduce_to_constant(te0t, m);
+    try_reduce_to_constant(te1t, m);
+    if(!is_constant(te0t)) return EXIT_FAILURE;
+    if(is_constant(te1t)) return EXIT_FAILURE;
+    
     }
     return EXIT_SUCCESS;
 }
