4f5bb78e806fddc3ccbe97db36845155304c8db2
Igor Krivenko <igor.s.krivenko@gmail.com>
2014-09-05 20:02:02 +0200

    A major simplification of callable_complex.

--- c++/callable_complex.hpp
+++ c++/callable_complex.hpp
@@ -1,177 +1,54 @@
 #pragma once
 
 #include <complex>
-#include <limits>
-#include <type_traits>
 #include <utility>
-#include <tuple>
 #include <ostream>
-#include <triqs/utility/draft/numeric_ops.hpp>
 #include <boost/serialization/complex.hpp>
-#include <boost/operators.hpp>
+#include <boost/serialization/base_object.hpp>
 
 namespace realevol {
 
-// Callable complex type
 template<class T>
-class callable_complex : public boost::operators<callable_complex<T>> {
-
-    T re, im;
-
-    template<typename X>
-    struct _T_compatible : public std::is_convertible<typename std::decay<X>::type,T> {};
-
-    template<typename X>
-    struct _is_complex : public std::integral_constant<bool,
-                std::is_same<typename std::decay<X>::type,callable_complex>::value ||
-                (triqs::is_complex<X>::value && _T_compatible<typename X::value_type>::value)
-                > {};
-
+class callable_complex : public std::complex<T> {
 public:
 
-    using value_type = T;
+    using base_t = std::complex<T>;
 
     // Constructors
-    callable_complex() : re(.0), im(.0) {}
-    template<typename R, typename I,
-             typename std::enable_if<_T_compatible<R>::value && _T_compatible<I>::value,bool>::type = false>
-    callable_complex(R r, I i) : re(r), im(i)
-    {}
-    template<typename C,
-             typename std::enable_if<triqs::is_complex<C>::value && _T_compatible<typename C::value_type>::value,
-             bool>::type = false>
-    callable_complex(C z) : re(z.real()), im(z.imag())
-    {}
-    template<typename R,
-             typename std::enable_if<_T_compatible<R>::value,bool>::type = false>
-    callable_complex(R r) : re(r), im(.0)
-    {}
+    callable_complex(T const& re = T(), T const& im = T()) : base_t(re,im) {};
+    callable_complex(callable_complex const& other) = default;
+    template<typename X> callable_complex(std::complex<X> const& x) : base_t(x) {};
 
-    // Copy-constructor, move-constructor & assignment
-    callable_complex(callable_complex const&) = default;
-    callable_complex(callable_complex &&) = default;
-    callable_complex & operator=(callable_complex const&) = default;
-
-    template<typename R,
-             typename std::enable_if<_T_compatible<R>::value,bool>::type = false>
-    callable_complex & operator=(R r)
-    {
-        re = r;
-        im = .0;
+    // Assignments
+    callable_complex& operator=(callable_complex const& other) = default;
+    callable_complex& operator=(T const& x) {
+        *static_cast<base_t*>(this) = x;
         return *this;
-    }
+    };
 
-    // Forward the call to the real and imaginary parts
+    // Call operator: forward the call to the real and imaginary parts
     template<typename... Args>
     auto operator()(Args&&... args) const -> std::complex<typename std::result_of<T(Args...)>::type>
     {
-        return {re(std::forward<Args>(args)...),im(std::forward<Args>(args)...)};
-    }
-
-    // Unary minus
-    callable_complex operator-() const
-    {
-        return callable_complex(-re,-im);
-    }
-
-    // Addition
-    template<typename C,typename std::enable_if<_is_complex<C>::value,bool>::type = false>
-    callable_complex operator+=(C z)
-    {
-        re += z.real();
-        im += z.imag();
-        return *this;
-    }
-
-    template<typename U,typename std::enable_if<_T_compatible<U>::value,bool>::type = false>
-    callable_complex operator+=(U x)
-    {
-        re += x.re;
-        return *this;
-    }
-
-    // Subtraction
-    template<typename C,typename std::enable_if<_is_complex<C>::value,bool>::type = false>
-    callable_complex operator-=(C z)
-    {
-        re -= z.real();
-        im -= z.imag();
-        return *this;
-    }
-
-    template<typename U,typename std::enable_if<_T_compatible<U>::value,bool>::type = false>
-    callable_complex operator-=(U x)
-    {
-        re -= x.re;
-        return *this;
-    }
-
-    // Multiplication
-    template<typename C,typename std::enable_if<_is_complex<C>::value,bool>::type = false>
-    callable_complex operator*=(C z)
-    {
-        std::tie(re,im) = std::make_tuple(re*z.real() - im*z.imag(),
-                                          re*z.imag() + im*z.real());
-        return *this;
-    }
-
-    template<typename U,typename std::enable_if<_T_compatible<U>::value,bool>::type = false>
-    callable_complex operator*=(U x)
-    {
-        re *= x;
-        im *= x;
-        return *this;
-    }
-
-    // Division
-    template<typename C,typename std::enable_if<_is_complex<C>::value,bool>::type = false>
-    callable_complex operator/=(C z)
-    {
-        T abs2 = z.real()*z.real() + z.imag()*z.imag();
-        std::tie(re,im) = std::make_tuple((re*z.real() + im*z.imag())/abs2,
-                                          (im*z.real() - re*z.imag())/abs2);
-        return *this;
-    }
-
-    template<typename U,typename std::enable_if<_T_compatible<U>::value,bool>::type = false>
-    callable_complex operator/=(U x)
-    {
-        re /= x;
-        im /= x;
-        return *this;
-    }
-
-    value_type real() const { return re; }
-    value_type imag() const { return im; }
-
-    bool operator==(callable_complex const& z) const
-    {
-        return re==z.re && im == z.im;
-    }
-
-    friend bool is_constant(callable_complex const& z)
-    {
-        return is_constant(z.re) && is_constant(z.im);
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, callable_complex const& z)
-    {
-        os << "(" << z.re << "," << z.im << ")";
-        return os;
+        return {base_t::real()(std::forward<Args>(args)...),
+                base_t::imag()(std::forward<Args>(args)...)};
     }
 
 private:
-
     // Boost serialization
     friend class boost::serialization::access;
     template<class Archive>
     void serialize(Archive & ar, const unsigned int version)
     {
-        // serialize base class information
-        ar & re;
-        ar & im;
+        // serialize base class
+        ar & boost::serialization::base_object<base_t>(*this);
     }
 };
+
+template<class T>
+bool is_constant(callable_complex<T> const& z){
+    return is_constant(std::real(z)) && is_constant(std::imag(z));
+}
 
 }
 
@@ -180,7 +57,7 @@ namespace triqs { namespace utility {
 template<typename T>
 bool is_zero(realevol::callable_complex<T> const& cte) {
     return triqs::utility::is_zero(cte.real()) &&
-        triqs::utility::is_zero(cte.imag());
+           triqs::utility::is_zero(cte.imag());
 }
 
 template<typename T>
