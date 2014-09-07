b9fad197a1ce789a8264d9db3da7f8a58248ce41
Igor Krivenko <iskrivenko@proton.me>
2014-09-07 14:22:21 +0200

    Python wrapper for time_expr

---
+++ python/realevol_desc.py
@@ -0,0 +1,28 @@
+from wrap_generator import *
+
+# The many_body_operators module
+module = module_(full_name = "pytriqs.applications.realevol", doc = "Real-time propagation solver")
+module.add_include("<triqs/arrays.hpp>")
+module.add_include("c++/time_expr.hpp")
+module.add_using("namespace realevol")
+
+# The operator class
+texpr = class_(
+        py_type = "texpr",
+        c_type = "time_expr",
+        c_type_absolute = "realevol::time_expr",
+        is_printable= True,
+        arithmetic = ("algebra","with_unit","with_unary_minus","double"),
+        doc = "Time-dependent expression (muParser wrapper)"
+        )
+
+texpr.add_constructor(signature="(std::string expr)", doc="create expression")
+texpr.add_constructor(signature="(double r = 0)", doc="create constant-valued expression")
+texpr.add_method("bool is_zero()", doc = "Boolean : is zero?")
+texpr.add_call(signature="double(double t)", doc="Substitute a time value into the expression")
+
+module.add_class(texpr)
+
+module.add_function(signature="bool is_constant(realevol::time_expr te)", doc="Boolean : is constant expression?")
+
+module.generate_code()
