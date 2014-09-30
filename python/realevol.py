c7a46f657b8f973ef5aea673e030a8efdd4331f9
Igor Krivenko <igor.s.krivenko@gmail.com>
2014-10-01 01:36:42 +0200

    Reorganization of Python part
    * Move all wrapped C++ types and functions into one module.
    * New universal factory object 'texpr' which automatically chooses
    between wrappers of time_expr/time_expr_c.

---
+++ python/realevol.py
@@ -0,0 +1,11 @@
+from realevol_core import texpr_r, texpr_c
+from realevol_core import is_constant, is_zero
+
+class texpr(object):
+    """Factory object for both real and complex time-dependent expressions"""
+    def __new__(texpr,*args):
+        arity = {0: texpr_r(),
+                 1: texpr_c(args[0]) if isinstance(args[0],complex) else texpr_r(args[0]),
+                 2: texpr_c(*args)}
+        if len(args) not in arity: raise TypeError("Too many arguments to construct texpr")
+        return arity[len(args)]
---
+++ python/realevol_core_desc.py
@@ -0,0 +1,52 @@
+from wrap_generator import *
+
+# texpr module
+module = module_(full_name = "pytriqs.applications.realevol.realevol_core", doc = "Time-dependent expression")
+module.add_include("<complex>")
+module.add_include("<triqs/arrays.hpp>")
+module.add_include("c++/time_expr.hpp")
+module.add_include("c++/c_time_expr.hpp")
+module.add_using("namespace realevol")
+module.add_using("triqs::utility::is_zero")
+
+# Real time-dependent expression
+texpr_r = class_(
+        py_type = "texpr_r",
+        c_type = "time_expr",
+        c_type_absolute = "realevol::time_expr",
+        is_printable= True,
+        arithmetic = ("algebra","with_unit","with_unary_minus","double","std::string"),
+        doc = "Time-dependent expression (ExprTk wrapper)"
+        )
+
+texpr_r.add_constructor(signature="(std::string expr)", doc="create expression")
+texpr_r.add_constructor(signature="(double r = 0)", doc="create constant-valued expression")
+texpr_r.add_call(signature="double(double t)", doc="Substitute a time value into the expression")
+module.add_class(texpr_r)
+
+module.add_function(signature="bool is_zero(time_expr te)", doc="Boolean : is zero expression?")
+module.add_function(signature="bool is_constant(time_expr te)", doc="Boolean : is constant expression?")
+
+# Complex time-dependent expression
+texpr_c = class_(
+        py_type = "texpr_c",
+        c_type = "c_time_expr",
+        c_type_absolute = "realevol::c_time_expr",
+        is_printable= True,
+        arithmetic = ("algebra","with_unit","with_unary_minus","time_expr","double","std::string","std::complex<double>"),
+        doc = "Complex time-dependent expression (ExprTk wrapper)"
+        )
+
+from itertools import product
+
+constructor_arg_types = ('double','std::string','time_expr')
+for arg_t1, arg_t2 in product(constructor_arg_types,constructor_arg_types):
+    texpr_c.add_constructor(signature="(%s re = %s(), %s im = %s())" %((arg_t1,arg_t1,arg_t2,arg_t2)), doc="create expression")
+texpr_c.add_constructor(signature="(std::complex<double> z)", doc="create expression")
+texpr_c.add_call(signature="std::complex<double>(double t)", doc="Substitute a time value into the expression")
+module.add_class(texpr_c)
+
+module.add_function(signature="bool is_zero(c_time_expr te)", doc="Boolean : is zero expression?")
+module.add_function(signature="bool is_constant(c_time_expr te)", doc="Boolean : is constant expression?")
+
+module.generate_code()
