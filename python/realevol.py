3a38953fbcce0ec322fb087f5bd88f141fd62da0
Igor Krivenko <iskrivenko@proton.me>
2014-10-01 17:12:28 +0200

    Finished with merging all Python modules together

--- python/realevol.py
+++ python/realevol.py
@@ -1,6 +1,8 @@
 from realevol_core import texpr_r, texpr_c
 from realevol_core import rmesh
-from realevol_core import is_constant, is_zero
+from realevol_core import Operator_r, c_r, c_dag_r, n_r
+from realevol_core import Operator_c, c_c, c_dag_c, n_c
+from realevol_core import is_constant, is_zero, dagger
 
 class texpr(object):
     """Factory object for both real and complex time-dependent expressions"""
@@ -10,3 +12,25 @@ class texpr(object):
                  2: texpr_c(*args)}
         if len(args) not in arity: raise TypeError("Too many arguments to construct texpr")
         return arity[len(args)]
+
+_complex_ops = False
+def use_complex_operators(use):
+    """Switch between real- and complex-valued fermionic operators"""
+    global _complex_ops
+    _complex_ops = use
+
+class Operator(object):
+    """Factory object for both real and complex fermionic operators"""
+    def __new__(Operator):
+        global _complex_ops
+        return Operator_c() if _complex_ops else Operator_r()
+
+def c(ind):
+    global _complex_ops
+    return c_c(ind) if _complex_ops else c_r(ind)
+def c_dag(ind):
+    global _complex_ops
+    return c_dag_c(ind) if _complex_ops else c_dag_r(ind)
+def n(ind):
+    global _complex_ops
+    return n_c(ind) if _complex_ops else n_r(ind)
---
+++ test/python/operators.py
@@ -0,0 +1,115 @@
+from pytriqs.applications.realevol.realevol import texpr_r, texpr_c
+from pytriqs.applications.realevol.realevol import use_complex_operators, Operator, c, c_dag, n, dagger
+from itertools import product
+
+def test_commutators(Cd,C):
+    print
+    print "Commutators:"
+    for cdi, ci in product(Cd,C):
+        print "[", cdi, ",", ci, "] =", cdi*ci - ci*cdi
+
+def test_anticommutators(Cd,C):
+    print
+    print "Anticommutators:"
+    for cdi, ci in product(Cd,C):
+        print "{", cdi, ",", ci, "} =", cdi*ci + ci*cdi
+
+print "Real-expression-valued operators"
+print "====================================="
+
+use_complex_operators(False)
+
+# Commutation relations
+C = [c(1), c(2), c(3)]
+Cd = [c_dag(1), c_dag(2), c_dag(3)]
+
+test_anticommutators(Cd,C)
+test_commutators(Cd,C)
+
+x = "t^2"*c(0)
+y = c_dag(1)
+
+print
+print "Algebra:"
+print "x =", x
+print "y =", y
+
+print "-x=", -x
+print
+print "x + 2.0 =", x + 2.0
+print "2.0 + x =", 2.0 + x
+print "x - 2.0 =", x - 2.0
+print "2.0 - x =", 2.0 - x
+print "3.0*y =", 3.0*y
+print "y*3.0 =", y*3.0
+print "x + 2.0*t =", x + "2*t"
+print "2.0*t + x =", "2*t" + x
+print "x - 2.0*t =", x - "2.0*t"
+print "2.0*t - x =", "2.0*t" - x
+print "3.0*t*y =", "3.0*t"*y
+print "y*3.0*t =", y*"3.0*t"
+print "x + y =", x + y
+print "x - y =", x - y
+print "(x + y)*(x - y) =", (x + y)*(x - y)
+
+print
+print "N^3:"
+N = n("up") + n("dn")
+N3 = N*N*N
+print "N =", N
+print "N^3 =", N3
+
+X = c_dag(1) * c_dag(2) * c(3) * c(4)
+print
+print "X =", X
+print "dagger(X) =", dagger(X)
+
+print "Complex-expression-valued operators"
+print "====================================="
+
+use_complex_operators(True)
+
+# Commutation relations
+C = [c(1), c(2), c(3)]
+Cd = [c_dag(1), c_dag(2), c_dag(3)]
+
+test_anticommutators(Cd,C)
+test_commutators(Cd,C)
+
+x = "t^2"*c(0)
+y = c_dag(1)
+
+print
+print "Algebra:"
+print "x =", x
+print "y =", y
+
+print "-x=", -x
+print
+print "x + 2.0*I =", x + 2.0*1j 
+print "2.0*I + x =", 2.0*1j + x 
+print "x - 2.0*I =", x - 2.0*1j
+print "2.0*I - x =", 2.0*1j - x 
+print "3.0*I*y =", (3.0*1j)*y 
+print "y*3.0*I =", y*(3.0*1j) 
+print "x + 2.0*t*I =", x + texpr_c("2*t")*1j 
+print "2.0*t*I + x =", texpr_c("2*t")*1j + x 
+print "x - 2.0*t*I =", x - texpr_c("2*t")*1j 
+print "2.0*t*I - x =", texpr_c("2.0*t")*1j - x 
+print "3.0*t*I*y =", texpr_c("3.0*t")*1j*y 
+print "y*3.0*I*t =", y*texpr_c("3.0*t")*1j
+print "x + y =", x + y
+print "x - y =", x - y
+print "(x + y)*(x - y) =", (x + y)*(x - y)
+
+print
+print "N^3:"
+N = n("up") + n("dn")
+N3 = N*N*N
+print "N =", N
+print "N^3 =", N3
+
+X = 1j*c_dag(1) * c_dag(2) * c(3) * c(4)
+print
+print "X =", X
+print "dagger(X) =", dagger(X)
