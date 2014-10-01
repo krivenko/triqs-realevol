5e96995534743b362da9132479c1449cefd96e3e
Igor Krivenko <igor.s.krivenko@gmail.com>
2014-10-01 17:12:28 +0200

    Finished with merging all Python modules together

--- python/__init__.py
+++ python/__init__.py
@@ -1,5 +1,9 @@
 from realevol import texpr
 from realevol import rmesh
+from realevol import use_complex_operators, Operator, c, c_dag, n, dagger
 from realevol import is_constant, is_zero
 
-__all__ = ['texpr','rmesh','is_constant','is_zero']
+__all__ = ['texpr',
+           'rmesh',
+           'use_complex_operators','Operator','c','c_dag','n','dagger',
+           'is_constant','is_zero']
