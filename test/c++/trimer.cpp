9106fc8be9083d844b7f69d8726253931042af1b
Igor Krivenko <iskrivenko@proton.me>
2014-12-21 22:39:45 +0100

    Done with test 'trimer'
    * There are still bugs in the solver itself.

--- test/c++/trimer.cpp
+++ test/c++/trimer.cpp
@@ -2,7 +2,10 @@
 
 #include <string>
 #include <array>
+#include <fstream>
+
 #include <triqs/gfs/meshes/segment.hpp>
+#include <triqs/h5.hpp>
 
 #include "../c++/time_expr_r.hpp"
 #include "../c++/solver.hpp"
@@ -13,11 +16,29 @@ using triqs::utility::many_body_operator;
 using triqs::utility::c;
 using triqs::utility::c_dag;
 using triqs::utility::n;
+using triqs::gfs::segment_mesh;
+
+namespace h5 = triqs::h5;
 
 double hbar = 1.0;
 double U = 1.0;
 double mu = 0.5*U;
 time_expr_r V = "0.3*(1 - exp(-4*t))";
+
+struct write_obs : public boost::static_visitor<> {
+
+    h5::group & gr;
+    std::string name;
+    write_obs(h5::group & gr, std::string name) : gr(gr), name(name) {}
+
+    template<typename MeshContainer>
+    void operator()(MeshContainer const& sol) const { h5_write(gr,name,sol); }
+};
+
+void write_results(var_results_t const& res, h5::group gr) {
+    for(auto const& r : res)
+        boost::apply_visitor(write_obs(gr,r.first), r.second);
+}
 
 // Real-valued version of the solver
 using solver_t = solver<false>;
@@ -50,7 +71,7 @@ int main()
     }
 
     // Time mesh
-    triqs::gfs::segment_mesh mesh(0,50.0,501);
+    segment_mesh mesh(0,50.0,501);
 
     // Solver object
     solver_t S(all_indices);
@@ -62,22 +83,41 @@ int main()
 
     // Observables
     std::map<std::string,operator_t<false>> observables;
+
     params.observables["unity"] = operator_t<false>() + 1.0; // ugly
+    for(auto a : atoms)
+        for(auto s : spins) {
+            auto name = "n_" + std::to_string(a) + "_" + s;
+            params.observables[name] = n<time_expr_r>(a,s);
+        }
+
+    h5::file output_file("trimer.output.h5",H5F_ACC_TRUNC);
+    h5::group root_gr(output_file);
 
     // psi0: Sz=1/2
+    {
     S.psi0({{1,"up"},{2,"up"},{3,"dn"}}) = 1.0;
     params.method = method_lanczos;
     S.solve(params);
-    // TODO
+
+    auto res = S.get_results();
+
+    auto gr = root_gr.create_group("Sz_0.5");
+    write_results(res,gr);
+    }
 
     // psi0: Sz=0
+    {
     S.psi0().amplitudes()() = .0;
-    //S.psi0({{1,"up"},{1,"dn"},{2,"up"},{3,"dn"}}) = 1.0;
-    S.psi0({{1,"up"},{1,"dn"},{2,"up"},{3,"dn"}}) = 1.0/sqrt(2.0);
-    S.psi0({{1,"up"},{2,"up"},{3,"dn"}}) = 1.0/sqrt(2.0);
+    S.psi0({{1,"up"},{1,"dn"},{2,"up"},{3,"dn"}}) = 1.0;
     params.method = method_runge_kutta;
     S.solve(params);
-    // TODO
+
+    auto res = S.get_results();
+
+    auto gr = root_gr.create_group("Sz_0");
+    write_results(res,gr);
+    }
 
     return EXIT_SUCCESS;
 }
