a75fb6b2d3e07bd7bae042c53540a1228c82d2a4
Igor Krivenko <iskrivenko@proton.me>
2014-12-24 17:49:32 +0100

    Split 'trimer' test into 2: trimer_lanczos and trimer_rk.

--- test/c++/trimer.hpp
+++ test/c++/trimer.hpp
@@ -1,30 +1,3 @@
-#include <triqs/utility/first_include.hpp>
-
-#include <string>
-#include <array>
-#include <fstream>
-
-#include <triqs/gfs/meshes/segment.hpp>
-#include <triqs/h5.hpp>
-
-#include "../c++/time_expr_r.hpp"
-#include "../c++/solver.hpp"
-
-using namespace realevol;
-
-using triqs::utility::many_body_operator;
-using triqs::utility::c;
-using triqs::utility::c_dag;
-using triqs::utility::n;
-using triqs::gfs::segment_mesh;
-
-namespace h5 = triqs::h5;
-
-double hbar = 1.0;
-double U = 1.0;
-double mu = 0.5*U;
-auto V = "0.3*(1 - exp(-4*t))"_te;
-
 struct write_obs : public boost::static_visitor<> {
 
     h5::group & gr;
@@ -91,13 +64,14 @@ int main()
             params.observables[name] = n<time_expr_r>(a,s);
         }
 
-    h5::file output_file("trimer.output.h5",H5F_ACC_TRUNC);
+    params.method = method;
+
+    h5::file output_file(output_filename.c_str(),H5F_ACC_TRUNC);
     h5::group root_gr(output_file);
 
     // psi0: Sz=1/2
     {
     S.psi0({{1,"up"},{2,"up"},{3,"dn"}}) = 1.0;
-    params.method = method_lanczos;
     S.solve(params);
 
     auto res = S.get_results();
@@ -110,7 +84,6 @@ int main()
     {
     S.psi0().amplitudes()() = .0;
     S.psi0({{1,"up"},{1,"dn"},{2,"up"},{3,"dn"}}) = 1.0;
-    params.method = method_runge_kutta;
     S.solve(params);
 
     auto res = S.get_results();
