7d88dba7b71cf8c5198849a6772623f9274876f4
Igor Krivenko <iskrivenko@proton.me>
2016-11-30 17:55:43 +0100

    [init_state] New class and a set of functions to prepare initial states.
    * Only make_pure_init_state() is implemented at the moment.

--- c++/init_state.cpp
+++ c++/init_state.cpp
@@ -21,11 +21,94 @@
 #include <triqs/utility/first_include.hpp>
 
 #include <cmath>
+#include <triqs/arrays/vector.hpp>
+#include <arpack/arpack_worker.hpp>
 
 #include "init_state.hpp"
-#include "lanczos_worker.hpp"
 
 namespace realevol {
+
+std::ostream & operator<<(std::ostream & os, init_state_t const& st) {
+ os << "Fundamental operator set" << std::endl;
+ os << "------------------------" << std::endl;
+ os << "Fermions: ";
+ for(auto it = st.fops.begin(Fermion); it != st.fops.end(Fermion); ++it)
+  os << "(" << it->index << ") ";
+ os << std::endl << "Bosons: ";
+ for(auto it = st.fops.begin(Boson); it != st.fops.end(Boson); ++it)
+  os << "(" << it->index << ") ";
+
+ os << std::endl << "Dimension of fermionic space: " << st.full_hs.size(Fermion) << std::endl;
+ os << "Dimension of bosonic space: " << st.full_hs.size(Boson) << std::endl;
+
+ os << st.sub_hilbert_spaces.size() << " relevant invariant subspaces with dimensions ";
+ for(auto const& sp : st.sub_hilbert_spaces) os << sp.size() << " ";
+ os << std::endl;
+
+ for(int i = 0; i < st.weighted_states.size(); ++i) {
+  auto const& s = st.weighted_states[i];
+  os << "State " << i << " (weight = " << s.weight << ") with parts in ";
+  for(auto const& p : s.parts) os << p.get_hilbert().get_index() << " ";
+  os << std::endl;
+ }
+
+ return os;
+}
+
+// -----------------------------------------------------------------
+
+void h5_write(h5::group fg, std::string const &name, init_state_t const& st) {
+ auto gr = fg.create_group(name);
+ gr.write_triqs_hdf5_data_scheme(st);
+
+ h5_write_attribute(gr, "fops", st.fops);
+ h5_write(gr, "full_hs", st.full_hs);
+ h5_write(gr, "sub_hilbert_spaces", st.sub_hilbert_spaces);
+
+ auto states_gr = gr.create_group("weighted_states");
+ for(int i = 0; i < st.weighted_states.size(); ++i) {
+  auto wst_gr = states_gr.create_group(std::to_string(i));
+  auto const& wst = st.weighted_states[i];
+
+  h5_write(wst_gr, "weight", wst.weight);
+
+  std::vector<unsigned long> sp_indices;
+  for(int p = 0; p < wst.parts.size(); ++p) {
+   sp_indices.push_back(wst.parts[p].get_hilbert().get_index());
+   h5_write(wst_gr, std::to_string(p), wst.parts[p].amplitudes());
+  }
+  h5_write(wst_gr, "sp_indices", sp_indices);
+ }
+}
+
+// -----------------------------------------------------------------
+
+void h5_read(h5::group fg, std::string const &name, init_state_t & st) {
+ auto gr = fg.open_group(name);
+
+ h5_read_attribute(gr, "fops", st.fops);
+ h5_read(gr, "full_hs", st.full_hs);
+ h5_read(gr, "sub_hilbert_spaces", st.sub_hilbert_spaces);
+
+ auto states_gr = gr.open_group("weighted_states");
+ int n_wst = states_gr.get_all_subgroup_names().size();
+ for(int i = 0; i < n_wst; ++i) {
+  st.weighted_states.emplace_back();
+  auto & wst = st.weighted_states[i];
+  auto wst_gr = states_gr.open_group(std::to_string(i));
+
+  h5_read(wst_gr, "weight", wst.weight);
+
+  std::vector<unsigned long> sp_indices;
+  h5_read(wst_gr, "sp_indices", sp_indices);
+  for(int p = 0; p < sp_indices.size(); ++p) {
+   wst.parts.push_back(st.sub_hilbert_spaces[sp_indices[p]]);
+   h5_read(wst_gr, std::to_string(p), wst.parts[p].amplitudes());
+  }
+ }
+}
+
+// -----------------------------------------------------------------
 
 static_operator_t make_static_op(operator_t const& op, std::string const& error_message) {
  static_operator_t static_op;
@@ -40,35 +123,45 @@ static_operator_t make_static_op(operator_t const& op, std::string const& error_
  return static_op;
 }
 
-std::vector<init_state_t> init_state_pure(operator_t const& generator, hilbert_space_structure & hss) {
+init_state_t make_pure_init_state(operator_t const& generator,
+                                  fundamental_operator_set const& fops,
+                                  std::map<operators::indices_t, int> const& bits_per_boson) {
 
  // Static version of generator
  auto gen = make_static_op(generator, "Generating operator must be time-independent!");
 
- std::vector<init_state_t> states = {{{}, 1.0}};
- auto & parts = states[0].parts;
+ init_state_t st(fops, bits_per_boson);
 
- state_on_space_t vacuum(hss.full_hs);
+ // Add one subspace with index 0
+ st.sub_hilbert_spaces.emplace_back(0);
+ auto & sp = st.sub_hilbert_spaces[0];
+
+ // Vaccum state in the full Hilbert space
+ state_on_space_t vacuum(st.full_hs);
  vacuum(fock_state_t(0)) = 1;
 
- static_op_on_space_t imp_gen(gen, hss.fops, hss.full_hs);
- auto init_state = imp_gen(vacuum);
+ // Imperative version of generator
+ static_op_on_space_t imp_gen(gen, st.fops, st.full_hs);
 
- foreach(init_state, [&parts,&hss](fock_state_t f, dcomplex a){
-  auto spn = hss.partition.lookup_basis_state(f);
-  auto const& sp = hss.sub_hilbert_spaces[spn];
-  auto ins = parts.emplace(spn, sp);
-  (ins.first->second)(sp.get_state_index(f)) = a;
- });
+ auto psi = imp_gen(vacuum);
 
- return states;
+ // Fill st.sub_hilbert_spaces[0]
+ foreach(psi, [&sp](fock_state_t f, dcomplex){ sp.add_fock_state(f); });
+
+ // Add a weighted state made of one part
+ state_on_subspace_t part(sp);
+ foreach(psi, [&part,&sp](fock_state_t f, dcomplex a){ part(sp.get_state_index(f)) = a; });
+ st.weighted_states.emplace_back(std::vector<state_on_subspace_t>{part}, 1.0);
+
+ return st;
 }
 
+/*
 std::vector<init_state_t> init_state_thermal(operator_t const& h0, hilbert_space_structure & hss,
                                              double beta, double temperature_cutoff) {
 
  // Static version the equilibrium Hamiltonian is static
- auto h0_ = make_static_op(h0, "Equilibrium Hamiltonian operator must be time-independent!");
+ auto h0_ = make_static_op(h0, "Equilibrium Hamiltonian must be time-independent!");
 
  // Hilbert space structure generated by the equilibrium Hamiltonian
  hilbert_space_structure hss0(h0, hss.fops, hss.full_hs, false);
@@ -79,21 +172,21 @@ std::vector<init_state_t> init_state_thermal(operator_t const& h0, hilbert_space
  // IEEE Transactions on Knowledge and Data Engineering, vol. 21, no. 8, pp. 1091-1103, Aug. 2009.
 
  // Iterate over all sectors of h0
- for(auto const& sp : hss0.sub_hilbert_spaces) {
-  static_op_on_subspace_t imp_h0(h0_, hss0.fops, hss.full_hs);
-
-  auto lw = lanczos_worker<static_op_on_subspace_t, state_on_subspace_t>(imp_h0,1e-10);
-
-  state_on_subspace_t psi0(sp); psi0(0) = 1.0;
-  lw(psi0);
-
-  std::cout << lw.values() << std::endl;
- }
+//  for(auto const& sp : hss0.sub_hilbert_spaces) {
+//   static_op_on_subspace_t imp_h0(h0_, hss0.fops, hss.full_hs);
+//
+//   auto lw = lanczos_worker<static_op_on_subspace_t, state_on_subspace_t>(imp_h0,1e-10);
+//
+//   state_on_subspace_t psi0(sp); psi0(0) = 1.0;
+//   lw(psi0);
+//
+//   std::cout << lw.values() << std::endl;
+//  }
 
  std::vector<init_state_t> states;
  // TODO
 
  return states;
 }
-
+*/
 }
