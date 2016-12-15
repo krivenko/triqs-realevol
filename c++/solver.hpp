01b28a55588a8e878c1813331f8b4832e08f92d1
Igor Krivenko <igor.s.krivenko@gmail.com>
2016-12-15 15:54:46 +0100

    [solver] Fleshing out compute_gf() and its Python wrapper

--- c++/solver.hpp
+++ c++/solver.hpp
@@ -37,30 +37,32 @@ namespace realevol {
 using namespace triqs::gfs;
 
 using indices_type = operators::indices_t;
-using g_2t_t = gf<cartesian_product<retime, retime>>;
+using gf_2t_t = gf<cartesian_product<retime, retime>>;
+using block_gf_2t_t = block_gf<cartesian_product<retime,retime>>;
 
 class solver {
 
- std::map<std::string, indices_type> gf_struct;           // Block structure of the Green function
- block_gf<cartesian_product<retime,retime>> g_l, g_g;     // Lesser and greater GF's to be calculated
- block_gf<cartesian_product<retime,retime>> g_ret, g_adv; // Retarded and advanced GF's to be calculated
- init_state const * initial_state = nullptr;              // Initial state at t=0
- triqs::mpi::communicator comm;                           // MPI communicator
- compute_gf_parameters_t compute_gf_params;               // Parameters of the last call to solve
- gf_mesh<retime> t_mesh;                                  // 1D time mesh to use in calculations
+ gf_struct_t gf_struct, chi_struct;          // Block structure of the Green functions and susceptibilities
+ block_gf_2t_t g_l, g_g;                     // Lesser and greater GF's to be calculated
+ block_gf_2t_t g_ret, g_adv;                 // Retarded and advanced GF's to be calculated
+ init_state const * initial_state = nullptr; // Initial state at t=t_min
+ triqs::mpi::communicator comm;              // MPI communicator
+ compute_gf_parameters_t compute_gf_params;  // Parameters of the last call to solve
+ gf_mesh<retime> t_mesh;                     // 1D time mesh to use in calculations
 
  // Make g_ret and g_adv out of g_l and g_g
  void make_gf_ret_adv();
 
 public:
 
- solver(std::map<std::string,indices_type> const& gf_struct, std::pair<double,double> time_window, int n_t = 1000);
+ solver(gf_struct_t const& gf_struct, gf_struct_t const& chi_struct,
+        std::pair<double,double> time_window, int n_t);
 
  /// Compute the Green's functions for given initial state and Hamiltonian
  TRIQS_WRAP_ARG_AS_DICT
  void compute_gf(compute_gf_parameters_t const& p);
 
- /// Get the initial state at t=0
+ /// Get the initial state at t=t_min
  init_state const& get_initial_state() const {
   if(initial_state == nullptr) TRIQS_RUNTIME_ERROR << "Initial state is not set!";
   return *initial_state;
@@ -73,16 +75,16 @@ public:
  compute_gf_parameters_t get_last_compute_gf_parameters() const { return compute_gf_params; }
 
  /// Lesser GF in real time
- block_gf_view<cartesian_product<retime,retime>> get_g_l() { return g_l; }
+ block_gf_2t_t get_g_l() { return g_l; }
 
  /// Greater GF in real time
- block_gf_view<cartesian_product<retime,retime>> get_g_g() { return g_g; }
+ block_gf_2t_t get_g_g() { return g_g; }
 
  /// Retarded GF in real time
- block_gf_view<cartesian_product<retime,retime>> get_g_ret() { return g_ret; }
+ block_gf_2t_t get_g_ret() { return g_ret; }
 
  /// Advanced GF in real time
- block_gf_view<cartesian_product<retime,retime>> get_g_adv() { return g_adv; }
+ block_gf_2t_t get_g_adv() { return g_adv; }
 };
 
 }
