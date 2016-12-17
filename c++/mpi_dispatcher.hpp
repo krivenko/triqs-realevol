c4cf09ac151b33dab20e1f8d815a998eb55c2ac3
Igor Krivenko <iskrivenko@proton.me>
2016-12-17 15:13:26 +0100

    [mpi_dispatcher] Redesign mpi_dispatcher
    * Make the class templated on the job descriptor type Job (any POD
      default-constructible type with mpi_datatype<Job>() being defined).
    * operator() returns Job instances.
    * If no jobs are left to distribute, operator() will throw a special
      exception mpi_dispatcher::no_jobs_left instead of returning -1.
    * Jobs are generated using a stored generator function (passed to
      constructor and called only on master). Old style constructors are
      also available.
    * Test is updated.

--- c++/mpi_dispatcher.hpp
+++ c++/mpi_dispatcher.hpp
@@ -23,78 +23,101 @@
 #include <vector>
 #include <numeric>
 #include <algorithm>
+#include <functional>
+#include <exception>
 
 #include <triqs/mpi/base.hpp>
 #include <triqs/utility/exceptions.hpp>
 
 namespace realevol {
 
-class mpi_dispatcher {
+template<typename Job> class mpi_dispatcher {
 
- std::vector<long> jobs;
+ std::function<Job(long)> job_generator; // To be called only on master!
+ long n_jobs;
  long next_job_index;
  triqs::mpi::communicator comm;
 
- static constexpr int request_tag = 1024;
- static constexpr int answer_tag = 1025;
+ static constexpr int request_job_tag = 1024;
+ static constexpr int send_job_tag = 1025;
+ static constexpr int no_jobs_left_tag = 1026;
 
  long serial() {
-  if(next_job_index >= jobs.size())
-   return no_jobs_left;
+  if(next_job_index < n_jobs)
+   return job_generator(next_job_index++);
   else
-   return jobs[next_job_index++];
+   throw(no_jobs_left());
  }
 
  long master() {
-  while(next_job_index < jobs.size()) {
-   MPI_Request r1, r2;
-   MPI_Irecv(nullptr, 0, MPI_INT, MPI_ANY_SOURCE, request_tag, comm.get(), &r1);
+  using triqs::mpi::mpi_datatype;
+  int n_workers = comm.size()-1;
+  while(next_job_index < n_jobs + n_workers) {
+   MPI_Request recv_r, send_r;
+   MPI_Irecv(nullptr, 0, MPI_INT, MPI_ANY_SOURCE, request_job_tag, comm.get(), &recv_r);
    MPI_Status stat;
-   MPI_Wait(&r1,&stat);
-   MPI_Isend(&jobs[next_job_index], 1, MPI_LONG, stat.MPI_SOURCE, answer_tag, comm.get(), &r2);
-   ++next_job_index;
+   MPI_Wait(&recv_r,&stat);
+   if(next_job_index < n_jobs) { // Sill have jobs to send
+    auto job = job_generator(next_job_index);
+    MPI_Isend(&job, 1, mpi_datatype<Job>(),
+              stat.MPI_SOURCE, send_job_tag, comm.get(), &send_r);
+    ++next_job_index;
+   } else { // Sending no_jobs_left
+    MPI_Isend(nullptr, 0, MPI_INT,
+              stat.MPI_SOURCE, no_jobs_left_tag, comm.get(), &send_r);
+    ++next_job_index;
+   }
   }
   comm.barrier();
-  return no_jobs_left;
+  throw(no_jobs_left());
  }
 
  long worker() {
-  MPI_Request r1, r2;
-  MPI_Isend(nullptr, 0, MPI_INT, 0, request_tag, comm.get(), &r1);
-  long jobid;
-  MPI_Irecv(&jobid, 1, MPI_LONG, 0, answer_tag, comm.get(), &r2);
-  MPI_Wait(&r2,MPI_STATUS_IGNORE);
-  if(jobid == no_jobs_left) comm.barrier();
-  return jobid;
+  using triqs::mpi::mpi_datatype;
+  MPI_Request send_r;
+  MPI_Request recv_r[2];
+  MPI_Isend(nullptr, 0, MPI_INT, 0, request_job_tag, comm.get(), &send_r);
+  Job job;
+  MPI_Irecv(&job, 1, mpi_datatype<Job>(), 0, send_job_tag, comm.get(), &recv_r[0]);
+  MPI_Irecv(nullptr, 0, MPI_INT, 0, no_jobs_left_tag, comm.get(), &recv_r[1]);
+  int i;
+  MPI_Waitany(2,recv_r,&i,MPI_STATUS_IGNORE);
+  if(i==0) {
+   MPI_Cancel(&recv_r[1]);
+   return job;
+  } else {
+   MPI_Cancel(&recv_r[0]);
+   comm.barrier();
+  }
+  throw(no_jobs_left());
  }
 
 public:
 
- static constexpr long no_jobs_left = -1;
+ using job_t = Job;
 
- mpi_dispatcher(triqs::mpi::communicator const& comm, long n_jobs) :
-  next_job_index(0), comm(comm) {
+ /// Construct using a general job generating function
+ template<typename JobGenerator>
+ mpi_dispatcher(triqs::mpi::communicator const& comm, JobGenerator job_generator, long n_jobs) :
+  job_generator(job_generator), n_jobs(n_jobs), next_job_index(0), comm(comm) {
   TRIQS_ASSERT(n_jobs > 0);
-  if(comm.rank() == 0) {
-   int n_workers = comm.size()-1;
-   jobs.resize(n_jobs+n_workers);
-   std::iota(jobs.begin(), jobs.end()-n_workers, 0);
-   std::fill(jobs.end()-n_workers, jobs.end(), long(no_jobs_left));
-  }
  }
 
- mpi_dispatcher(triqs::mpi::communicator const& comm, std::vector<long> const& jobs_) :
-  next_job_index(0), comm(comm) {
-  TRIQS_ASSERT(jobs_.size() > 0);
-  if(comm.rank() == 0) {
-   int n_workers = comm.size()-1;
-   jobs.resize(jobs_.size()+n_workers);
-   std::copy(jobs_.begin(), jobs_.end(), jobs.begin());
-   std::fill(jobs.end()-n_workers, jobs.end(), long(no_jobs_left));
-  }
+ /// Construct and generate jobs as Job::Job(i), i = 0,..., n_jobs-1
+ mpi_dispatcher(triqs::mpi::communicator const& comm, long n_jobs) :
+  job_generator([](long i){ return job_t(i); }),
+  n_jobs(n_jobs), next_job_index(0), comm(comm) {
+  TRIQS_ASSERT(n_jobs > 0);
  }
 
- long operator()() {
+ /// Construct on a list of jobs
+ mpi_dispatcher(triqs::mpi::communicator const& comm, std::vector<Job> const& jobs) :
+  job_generator([jobs](long i){ return jobs[i]; }),
+  n_jobs(jobs.size()), next_job_index(0), comm(comm) {
+  TRIQS_ASSERT(jobs.size() > 0);
+ }
+
+ Job operator()() {
   if(comm.size() == 1) return serial();
   else {
    if(comm.rank() == 0) return master();
@@ -102,6 +125,7 @@ public:
   }
  }
 
+ class no_jobs_left : std::exception {};
 };
 
 }
