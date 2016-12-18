fe0d601feb8c869e0209ccb68c642b047fd276c5
Igor Krivenko <igor.s.krivenko@gmail.com>
2016-12-18 20:03:55 +0100

    [mpi_dispatcher] Allow passing extra arguments to job generator.
    * Fix return type of serial()/slave()/master().
    * Many smaller changes and optimizations.

--- c++/mpi_dispatcher.hpp
+++ c++/mpi_dispatcher.hpp
@@ -27,68 +27,92 @@
 #include <exception>
 
 #include <triqs/mpi/base.hpp>
+#include <triqs/utility/tuple_tools.hpp>
 #include <triqs/utility/exceptions.hpp>
 
 namespace realevol {
 
-template<typename Job> class mpi_dispatcher {
+template<typename Job, typename... GeneratorArgs>
+class mpi_dispatcher {
 
- std::function<Job(long)> job_generator; // To be called only on master!
+ // Job generating function; To be called only on master!
+ std::function<Job(long, GeneratorArgs...)> job_generator;
+ // Number of jobs to be generated
  long n_jobs;
- long next_job_index;
+ // Job index to be sent next
+ long next_job_index = 0;
+ // MPI datatype for Job
+ MPI_Datatype job_datatype = triqs::mpi::mpi_datatype<Job>();
+
  triqs::mpi::communicator comm;
 
- static constexpr int request_job_tag = 1024;
- static constexpr int send_job_tag = 1025;
- static constexpr int no_jobs_left_tag = 1026;
+ enum tags : int {request_job_tag = 1024,
+                  send_job_tag,
+                  generator_arg_tag};
 
- long serial() {
+ Job serial(GeneratorArgs... args) {
   if(next_job_index < n_jobs)
-   return job_generator(next_job_index++);
+   return job_generator(next_job_index++, args...);
   else
    throw(no_jobs_left());
  }
 
- long master() {
-  using triqs::mpi::mpi_datatype;
+ Job master(GeneratorArgs... args) {
   int n_workers = comm.size()-1;
+  MPI_Request recv_r;
+  MPI_Recv_init(nullptr, 0, MPI_INT, MPI_ANY_SOURCE, request_job_tag, comm.get(), &recv_r);
   while(next_job_index < n_jobs + n_workers) {
-   MPI_Request recv_r, send_r;
-   MPI_Irecv(nullptr, 0, MPI_INT, MPI_ANY_SOURCE, request_job_tag, comm.get(), &recv_r);
    MPI_Status stat;
-   MPI_Wait(&recv_r,&stat);
-   if(next_job_index < n_jobs) { // Sill have jobs to send
-    auto job = job_generator(next_job_index);
-    MPI_Isend(&job, 1, mpi_datatype<Job>(),
-              stat.MPI_SOURCE, send_job_tag, comm.get(), &send_r);
-    ++next_job_index;
-   } else { // Sending no_jobs_left
-    MPI_Isend(nullptr, 0, MPI_INT,
-              stat.MPI_SOURCE, no_jobs_left_tag, comm.get(), &send_r);
-    ++next_job_index;
+   MPI_Start(&recv_r);
+   MPI_Wait(&recv_r, &stat);
+
+   auto args = std::tuple<GeneratorArgs...>();
+   triqs::tuple::for_each(args, [this,&stat](auto & arg) {
+    using triqs::mpi::mpi_datatype;
+    auto arg_datatype = mpi_datatype<std14::decay_t<decltype(arg)>>();
+    MPI_Recv(&arg, 1, arg_datatype, stat.MPI_SOURCE, generator_arg_tag, comm.get(),
+             MPI_STATUS_IGNORE);
+   });
+
+   MPI_Request send_r;
+   if(next_job_index < n_jobs) { // Still have jobs to send
+    auto job = triqs::tuple::apply(job_generator,
+                                   std::tuple_cat(std::make_tuple(next_job_index), args));
+    MPI_Isend(&job, 1, job_datatype, stat.MPI_SOURCE, send_job_tag, comm.get(), &send_r);
+   } else {                      // Sending empty message
+    MPI_Isend(nullptr, 0, job_datatype, stat.MPI_SOURCE, send_job_tag, comm.get(), &send_r);
    }
+   ++next_job_index;
   }
+  MPI_Request_free(&recv_r);
+
   comm.barrier();
   throw(no_jobs_left());
  }
 
- long worker() {
-  using triqs::mpi::mpi_datatype;
+ Job worker(GeneratorArgs... args) {
+  // Notify master node
   MPI_Request send_r;
-  MPI_Request recv_r[2];
   MPI_Isend(nullptr, 0, MPI_INT, 0, request_job_tag, comm.get(), &send_r);
+  MPI_Wait(&send_r,MPI_STATUS_IGNORE);
+
+  // Send arguments of the job generator
+  using triqs::mpi::mpi_datatype;
+  triqs::tuple::for_each(std::make_tuple(args...), [this](auto arg) {
+   MPI_Send(&arg, 1, mpi_datatype<decltype(arg)>(), 0, generator_arg_tag, comm.get());
+  });
+
+  // Receive new job
   Job job;
-  MPI_Irecv(&job, 1, mpi_datatype<Job>(), 0, send_job_tag, comm.get(), &recv_r[0]);
-  MPI_Irecv(nullptr, 0, MPI_INT, 0, no_jobs_left_tag, comm.get(), &recv_r[1]);
-  int i;
-  MPI_Waitany(2,recv_r,&i,MPI_STATUS_IGNORE);
-  if(i==0) {
-   MPI_Cancel(&recv_r[1]);
-   return job;
-  } else {
-   MPI_Cancel(&recv_r[0]);
-   comm.barrier();
-  }
+  MPI_Request recv_r;
+  MPI_Status recv_s;
+  MPI_Irecv(&job, 1, job_datatype, 0, send_job_tag, comm.get(), &recv_r);
+  MPI_Wait(&recv_r,&recv_s);
+  int c;
+  MPI_Get_count(&recv_s, job_datatype,&c);
+  if(c == 1) return job;
+
+  comm.barrier();
   throw(no_jobs_left());
  }
 
@@ -96,35 +120,40 @@ public:
 
  using job_t = Job;
 
- /// Construct using a general job generating function
+ /// Construct using a general job generator
  template<typename JobGenerator>
  mpi_dispatcher(triqs::mpi::communicator const& comm, JobGenerator job_generator, long n_jobs) :
-  job_generator(job_generator), n_jobs(n_jobs), next_job_index(0), comm(comm) {
+  job_generator(job_generator), n_jobs(n_jobs), comm(comm) {
   TRIQS_ASSERT(n_jobs > 0);
  }
 
- /// Construct and generate jobs as Job::Job(i), i = 0,..., n_jobs-1
+ /// Construct and generate jobs as Job::Job(i, args...), i = 0,..., n_jobs-1
  mpi_dispatcher(triqs::mpi::communicator const& comm, long n_jobs) :
-  job_generator([](long i){ return job_t(i); }),
-  n_jobs(n_jobs), next_job_index(0), comm(comm) {
+  job_generator([](long i, GeneratorArgs... args){ return job_t(i, args...); }),
+  n_jobs(n_jobs), comm(comm) {
   TRIQS_ASSERT(n_jobs > 0);
  }
 
  /// Construct on a list of jobs
  mpi_dispatcher(triqs::mpi::communicator const& comm, std::vector<Job> const& jobs) :
-  job_generator([jobs](long i){ return jobs[i]; }),
-  n_jobs(jobs.size()), next_job_index(0), comm(comm) {
+  job_generator([jobs](long i, GeneratorArgs...){ return jobs[i]; }),
+  n_jobs(jobs.size()), comm(comm) {
   TRIQS_ASSERT(jobs.size() > 0);
  }
 
- Job operator()() {
-  if(comm.size() == 1) return serial();
+ Job operator()(GeneratorArgs... args) throw (no_jobs_left) {
+  if(comm.size() == 1) return serial(args...);
   else {
-   if(comm.rank() == 0) return master();
-   else                 return worker();
+   if(comm.rank() == 0) return master(args...);
+   else                 return worker(args...);
   }
  }
 
+ // Restart job generation
+ void reset() { next_job_index = 0; }
+
+ // Throw it from operator()
+ // C++17's std::optional would be a better solution.
  class no_jobs_left : std::exception {};
 };
 
