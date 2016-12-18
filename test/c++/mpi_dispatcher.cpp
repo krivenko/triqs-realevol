5eadfe6926e5b007eba8fc55448001fe240ec66d
Igor Krivenko <iskrivenko@proton.me>
2016-12-18 20:03:55 +0100

    [mpi_dispatcher] Allow passing extra arguments to job generator.
    * Fix return type of serial()/slave()/master().
    * Many smaller changes and optimizations.

--- test/c++/mpi_dispatcher.cpp
+++ test/c++/mpi_dispatcher.cpp
@@ -13,18 +13,24 @@ int main(int argc, char **argv) {
  return RUN_ALL_TESTS();
 }
 
-int test_sum(mpi_dispatcher<long> & disp, triqs::mpi::communicator const& comm) {
- int sum = 0;
- std::chrono::milliseconds sleep_duration(0);
+using namespace std::chrono;
+using namespace std::chrono_literals;
+using std::this_thread::sleep_for;
+
+template<typename Job = long, typename... GeneratorArgs>
+Job test_sum(mpi_dispatcher<Job,GeneratorArgs...> & disp,
+             triqs::mpi::communicator const& comm,
+             GeneratorArgs... args) {
+ Job sum = 0;
+ milliseconds sleep_duration(0);
  try {
   while(true) {
-   auto job = disp();
-   std::this_thread::sleep_for(sleep_duration);
-   using namespace std::chrono_literals;
+   auto job = disp(args...);
+   sleep_for(sleep_duration);
    sleep_duration += 1ms;
    sum += job;
   }
- } catch(mpi_dispatcher<long>::no_jobs_left &) {}
+ } catch(typename mpi_dispatcher<Job,GeneratorArgs...>::no_jobs_left &) {}
  sum = mpi_all_reduce(sum, comm, 0, MPI_SUM);
 
  return sum;
@@ -70,4 +76,32 @@ TEST(mpi_dispatcher, generator_undersub) {
  auto generator = [](long n) { return n*n; };
  mpi_dispatcher<long> disp(comm, generator, 2);
  EXPECT_EQ(1, test_sum(disp, comm));
+}
+
+TEST(mpi_dispatcher, generator_args) {
+ triqs::mpi::communicator comm;
+ auto generator = [](long n, int p, int r) { return p*n + 10*r; };
+ int N = 50, p = 3, r = 8;
+ mpi_dispatcher<long, int, int> disp(comm, generator, N);
+ EXPECT_EQ(p*N*(N-1)/2 + 10*r*N, test_sum(disp, comm, p, r));
+}
+
+TEST(mpi_dispatcher, generator_args_undersub) {
+ triqs::mpi::communicator comm;
+ auto generator = [](long n, int p, int r) { return p*n + 10*r; };
+ int N = 2, p = 3, r = 8;
+ mpi_dispatcher<long, int, int> disp(comm, generator, N);
+ EXPECT_EQ(p*N*(N-1)/2 + 10*r*N, test_sum(disp, comm, p, r));
+}
+
+TEST(mpi_dispatcher, complex) {
+ triqs::mpi::communicator comm;
+ mpi_dispatcher<dcomplex, double> disp(comm, 50);
+ EXPECT_CLOSE(dcomplex(1225,150), test_sum(disp, comm, 3.0));
+}
+
+TEST(mpi_dispatcher, complex_undersub) {
+ triqs::mpi::communicator comm;
+ mpi_dispatcher<dcomplex, double> disp(comm, 2);
+ EXPECT_CLOSE(dcomplex(1,6), test_sum(disp, comm, 3.0));
 }
