bf6b8e8e763b535a758dc9e7ccfa5f64e6139fc2
Igor Krivenko <igor.s.krivenko@gmail.com>
2017-01-13 00:35:56 +0300

    [test] Updated 'star' test to include a dynamical Hamiltonian.

--- test/python/star.py
+++ test/python/star.py
@@ -15,10 +15,10 @@ spin_names = ('up','dn')
 # Model parameters
 U = 3.0
 mu = U*0.5
-dmu = 0.1
 n_bath = 2
 eps = np.linspace(-0.2,0.2,n_bath)
 t = [0.3]*n_bath
+dt = te("0.1*(1-exp(-5*t))")
 
 fops = set(product(spin_names,range(n_bath+1)))
 gf_struct = {'dn':[0], 'up':[0]}
@@ -40,7 +40,7 @@ init_state = make_equilibrium_init_state(h0,
                                          params = {'verbosity':2})
 
 # Hamiltonian after quench
-h = h0 -dmu*(n('up',0) + n('dn',0))
+h = h0 + sum(dt*(c_dag(sn,0)*c(sn,1) + c_dag(sn,1)*c(sn,0)) for sn in spin_names)
 
 # Solver object
 S = Solver(gf_struct, chi_indices, t_max = t_max, n_t = n_t)
