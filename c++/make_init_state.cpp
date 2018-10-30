03def555b3796542dcc89b73929428aa732e6d0a
Michael Danilov <mike402>
2018-10-30 18:55:14 +0100

    make_init_state: Increase energy_window for zero temperature.

--- c++/make_init_state.cpp
+++ c++/make_init_state.cpp
@@ -432,7 +432,9 @@ init_state make_equilibrium_init_state(OperatorType const& h,
 
  double beta, energy_window;
  if(temperature == 0)
-  energy_window = (params.arpack_tolerance != 0 ? params.arpack_tolerance : 1e-14);
+  // XXX If this is too low, n_relevant_ev is "0" for zero temperature with ARPACK and MPI.
+  // The minimum tolerance that worked on my machine was "1e-13", for "1e-14" there was this problem. mdanilov
+  energy_window = (params.arpack_tolerance != 0 ? params.arpack_tolerance : 1e-9);
  else {
   beta = 1 / temperature;
   energy_window = -temperature * std::log(params.min_rel_weight);
